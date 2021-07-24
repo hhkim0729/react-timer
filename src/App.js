80a8335ed7e6ae9d09aeda5362a4d02e6b017b3c
hhkim0729 <vvv8506@gmail.com>
2021-07-24 16:00:33 +0900

    Fix routing error

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import './App.css';
 function App() {
   return (
     <div className="app">
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Route path="/" exact={true} component={Input} />
         <Route path="/time" component={Time} />
       </BrowserRouter>
